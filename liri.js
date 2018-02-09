25cdf577921fd6b7a006f2754b6932ff78cf9ec3
Caitlin Everett <pwt783@mac1s-mbp-68.attlocal.net>
2018-02-08 23:27:04 -0600

    got movie / request working

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ const keys = require('./keys');
 const Twitter = require("twitter");
 const Spotify = require('node-spotify-api');
 const fs = require('fs');
+const request = require('request');
 
 const arg2 = process.argv[2];
 const arg3 = process.argv[3];
@@ -16,11 +17,16 @@ function grabTweets(user) {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             tweets.forEach((val, index) => {
-                let content = `
+
+let content = `
+---------------------
+
 Tweet ${index}
 Created: ${val.created_at}
 Text: ${val.text}
-                `;
+
+---------------------
+`;
                 console.log(content);
 
 
@@ -36,37 +42,71 @@ Text: ${val.text}
 }
 
 function spotifyThis(song) {
-    console.log("spotify me", song); 
     var spotify = new Spotify(keys.spotify);
     
     spotify.search({ type: 'track', query: song }, function(err, data) {
     if (err) {
         return console.log('Error occurred: ' + err);
     }
+    
+    var firstTrack = data.tracks.items[0];
 
-    //console.log(data.tracks.items[0]); 
-    console.log(data.tracks.items[0].album.artists[0].name);
-    console.log(data.tracks.items[0].name);
-    console.log(data.tracks.items[0].album.name);
-    console.log(data.tracks.items[0].preview_url);
-    console.log("");
+let track = `
+---------------------
+
+Artist: ${firstTrack.album.artists[0].name}
+Song Name: ${firstTrack.name}
+Album Name: ${firstTrack.album.name}
+Preview URL: ${firstTrack.preview_url}
+
+---------------------
+ `;
+
+    console.log(track);
 
     // fs.writeFile("testoutput.json", JSON.stringify(data.tracks.items[0]), function(err) {
     //     if(err) {
     //         return console.log(err);
     //     }
-     
     //     //console.log("The file was saved!");
-    //  });
+    //  });`
 });
-
 }
+
+function movieThis(movie){
+    request(`http://www.omdbapi.com/?i=tt3896198&apikey=92231f0e&t=${movie}`, { json: true }, (err, res, body) => {
+
+let movie = `
+---------------------
+
+Title: ${body.Title}
+Release Year: ${body.Year}
+IMDB Rating: ${body.Ratings[0].Value}
+Rotton Tomatoes: ${body.Ratings[1].Value}
+Country of Origin: ${body.Country}
+Language: ${body.Language}
+Plot: ${body.Plot}
+Cast: ${body.Actors}
+
+---------------------
+ `;
+
+        console.log(movie);
+
+        if (err) { return console.log(err); }
+      });
+}
+
+
 switch (arg2) {
     case "my-tweets":
         grabTweets(arg3);
         break;
     case "spotify-this":
         spotifyThis(arg3);
+        break;
+    case "movie-this":
+        movieThis(arg3);
         break;
     default:
         console.log("error: incorrect command line argument");
